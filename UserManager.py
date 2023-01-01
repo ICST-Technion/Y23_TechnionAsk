ce2504f7b676edd3e8b9376663662853e5b3c5a7
Malik Khalaf <malik_khalaf@apple.com>
2023-01-01 16:47:12 +0200

    Added UserManager.

---
+++ UserManager.py
@@ -0,0 +1,19 @@
+from DatabaseManager import DatabaseManager
+class UserManager:
+    def __init__(self, db_manager : DatabaseManager):
+        self.db_manager = db_manager
+
+    def insertUser(self, name, password):
+        return self.db_manager.insert('accounts (username, password)', "('" + name + "', '" + password + "'")
+
+
+    def getUserId(self, name):
+        user_id = self.db_manager.select('accounts', 'Name = ' + name, 'userid')
+        return None
+
+    def authenticateUser(self, name, password):
+        user_password = self.db_manager.select('accounts', 'Name = ' + name, 'password')
+        if user_password is not None:
+            if password == user_password:
+                return True
+        return False
